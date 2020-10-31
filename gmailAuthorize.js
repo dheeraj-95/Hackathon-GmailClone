35d016026fbc47193e961d5a6c6a67ff78bd48d5
dheeraj <gumudavellidheeraj@gmail.com>
2020-10-31 21:12:35 +0530

    constructed json for pagination

--- gmailAuthorize.js
+++ gmailAuthorize.js
@@ -1,75 +1,85 @@
-// Enter an API key from the Google API Console:
-//   https://console.developers.google.com/apis/credentials
-var apiKey = 'AIzaSyD3SZ_hqSpRKuD0Io916-rt_k-OaZwOjZg';
+let clientId = "384837060954-tignegqj2b5jos63abs8crqma12pjkng.apps.googleusercontent.com";
+let apiKey = "AIzaSyD3SZ_hqSpRKuD0Io916-rt_k-OaZwOjZg";
 
-// Enter the API Discovery Docs that describes the APIs you want to
-// access. In this example, we are accessing the People API, so we load
-// Discovery Doc found here: https://developers.google.com/people/api/rest/
-var discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
+let getMails = document.getElementById('get-Mails');
 
-// Enter a client ID for a web application from the Google API Console:
-//   https://console.developers.google.com/apis/credentials?project=_
-// In your API Console project, add a JavaScript origin that corresponds
-//   to the domain where you will be running the script.
-var clientId = '384837060954-tignegqj2b5jos63abs8crqma12pjkng.apps.googleusercontent.com';
+let scopes = "https://mail.google.com/ https://www.googleapis.com/auth/gmail.addons.current.message.action https://www.googleapis.com/auth/gmail.addons.current.message.readonly https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.addons.current.action.compose";
 
-// Enter one or more authorization scopes. Refer to the documentation for
-// the API or https://developers.google.com/people/v1/how-tos/authorizing
-// for details.
-var scopes = 'profile';
-
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-
-function handleClientLoad() {
-    // Load the API client and auth2 library
-    gapi.load('client:auth2', initClient);
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({ scope: scopes })
+        .then(function (res) { console.log("Sign-in successful"); alert(`Hello ${res.tt.gV}! Logged In Successfully.`) },
+            function (err) { console.error("Error signing in", err); });
+}
+function loadClient() {
+    gapi.client.setApiKey(apiKey);
+    return gapi.client.load("https://gmail.googleapis.com/$discovery/rest?version=v1")
+        .then(function () {
+            console.log("GAPI client loaded for API");
+            getMails.classList.remove('d-none');
+            getMails.classList.add('d-inline');
+        },
+            function (err) { console.error("Error loading GAPI client for API", err); });
 }
 
-function initClient() {
-    gapi.client.init({
-        apiKey: apiKey,
-        discoveryDocs: discoveryDocs,
-        clientId: clientId,
-        scope: scopes
-    }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+async function execute() {
+    try {
+        let msgs = await gapi.client.gmail.users.messages.list({
+            "userId": "gumudavellidheeraj@gmail.com",
+            "includeSpamTrash": false,
+            "maxResults": 20,
+            "q": "category:social"
+        });
 
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        let msgResponse = JSON.parse(JSON.stringify(msgs.result.messages));
+        // console.log(msgResponse);
+        let resultArray = [];
+        resultArray = msgResponse.map(obj => { return obj["id"] })
 
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
-    });
-}
-
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'inline';
-        makeApiCall();
-    } else {
-        authorizeButton.style.display = 'inline';
-        signoutButton.style.display = 'none';
+        let resultMails = [];
+        for (let j = 0; j < resultArray.length; j++) {
+            let res = await getMailObj(resultArray[j]);
+            resultMails.push(res);
+        }
+        console.log(resultMails);
+    } catch (e) {
+        console.log('Error in Execute function', e);
     }
 }
 
-function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-}
+async function getMailObj(msgId) {
+    let resultObj = { from: null, subject: null, snippet: null, time: null }
+    try {
 
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
+        let response = await gapi.client.gmail.users.messages.get({
+            "userId": "gumudavellidheeraj@gmail.com",
+            "id": msgId,
+            "format": "full"
+        });
+        let mailDate = new Date(parseInt(response.result.internalDate)).toString().split(" ");
+        
+        if(new Date().getDay() > new Date(parseInt(response.result.internalDate)).getDay() ){
+            mailDate = mailDate[1] + ' ' + mailDate[2];
+            resultObj.time = mailDate;
+        }else{
+            let mailTime = new Date(parseInt(response.result.internalDate)).getHours() + ':' + new Date(parseInt(response.result.internalDate)).getMinutes();
+            resultObj.time = mailTime;
+        }
 
-// Load the API and make an API call.  Display the results on the screen.
-function makeApiCall() {
-    gapi.client.people.people.get({
-        'resourceName': 'people/me',
-        'requestMask.includeField': 'person.names'
-    }).then(function (resp) {
-        var name = resp.result.names[0].givenName;
-        alert('Hello, ' + name + '! You Have logged in Successfully.')
-    });
+        resultObj.snippet = response.result.snippet;
+        let data = JSON.parse(JSON.stringify(response.result.payload.headers));
+        data.filter(item => {
+            if (item["name"] === "Subject")
+                resultObj.subject = item["value"];
+            if (item["name"] === "From")
+                resultObj.from = item["value"];
+        });
+        return resultObj;
+    } catch (e) {
+        console.log('Error in getMailObj Function', e);
+    }
 }
+gapi.load("client:auth2", function () {
+    gapi.auth2.init({ client_id: clientId });
+});
+
